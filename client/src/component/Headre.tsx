609c5d090b08cdc73546f8dd99b182f6af717b2b
seif1000 <seifwin74@gmail.com>
2023-06-16 16:09:46 +0100

    completeing the header

--- client/src/component/Headre.tsx
+++ client/src/component/Headre.tsx
@@ -21,7 +21,7 @@ const Header = () => {
               <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-[#30b4a5] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -44,7 +44,7 @@ const Header = () => {
                     />
                   </div>
                   <div className="hidden sm:ml-6 sm:block">
-                    <div className="mt-2 flex space-x-4">
+                    <div className="mt-1 flex space-x-4">
                       {navigation.map((item) => (
                         <Link
                           key={item.name}
@@ -74,20 +74,18 @@ const Header = () => {
             <Disclosure.Panel className="sm:hidden">
               <div className="space-y-1 px-2 pb-3 pt-2">
                 {navigation.map((item) => (
-                  <Disclosure.Button
+                  <Link
                     key={item.name}
-                    as="a"
-                    href={item.href}
+                    to={item.href}
                     className={classNames(
                       item.current
-                        ? "bg-gray-900 text-white"
+                        ? "bg-[#30b4a5] text-white"
                         : "text-gray-300 hover:bg-gray-700 hover:text-white",
                       "block rounded-md px-3 py-2 text-base font-medium"
                     )}
-                    aria-current={item.current ? "page" : undefined}
                   >
                     {item.name}
-                  </Disclosure.Button>
+                  </Link>
                 ))}
               </div>
             </Disclosure.Panel>
